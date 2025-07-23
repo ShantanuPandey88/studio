d85651c6864e0d9c6c965916301b9dedfd292cb1
Shantanu pandey <santanupandey@gmail.com>
2025-07-23 08:16:01 +0000

    Set up Firebase Backend

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,6 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Poppins:wght@700&display=swap" rel="stylesheet" />
-        <Script src="/firebase-config.js" strategy="beforeInteractive" />
       </head>
       <body className="font-body antialiased">
           <AuthProvider>
